5f14f2843c99433f8fcfc8374f0e83cb99020e5f
engdiogenes <149336770+engdiogenes@users.noreply.github.com>
2025-05-07 09:12:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ credentials = {
     "usernames": {
         "energiajlr": {
             "name": "energiajlr",
-            "password": "123"
+            "password": "2b$12$KIXQZKZVZz8ZkZz8ZkZz8uZz8ZkZz8ZkZz8ZkZz8ZkZz8ZkZz8ZK"
         }
     }
 }
