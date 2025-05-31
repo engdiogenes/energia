3f62be58f531e238d46d8b482b4dacd719f150f2
engdiogenes <149336770+engdiogenes@users.noreply.github.com>
2025-05-31 11:38:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -130,6 +130,14 @@ with st.sidebar:
 
     if origem_dados == "Google Sheets":
         dados_colados = obter_dados_do_google_sheets()
+        # Converter os dados colados em DataFrame temporário para extrair a última data
+        df_temp = pd.read_csv(io.StringIO(limpar_valores(dados_colados)), sep="\t")
+        df_temp["Datetime"] = pd.to_datetime(df_temp["Date"] + " " + df_temp["Time"], dayfirst=True)
+        ultima_data = df_temp["Datetime"].max()
+
+        # Exibir no Streamlit
+        st.sidebar.markdown(f"📅 **Última atualização:** {ultima_data.strftime('%d/%m/%Y %H:%M')}")
+
     else:
         dados_colados = st.text_area("Cole os dados aqui (tabulados):", height=300)
 
